8adfde16a7ad2a7a2fca8aede0aa275445f7904d
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-03-29 20:11:51 +0530

    dokcer file for streamlit

---
+++ streamlit/hello_dev.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def main():
+    st.title("Welcome to Streamlit")
+    st.markdown("Hello :green[Developer], this is **:red[Streamlit]** inside **:blue[Docker]**.")
+    
+if __name__ == '__main__':
+    main()
--- streamlit/hello_world.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-
-st.markdown("Hello :green[Developer], this is **:red[Streamlit]** insider **:blue[Docker]**.")
